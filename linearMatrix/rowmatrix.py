c59de6a66c475ddce3b08fc6457ffbb54086c5e1
Sam Li <samlee2015jp@gmail.com>
2022-01-24 22:51:37 +0900

    add

---
+++ linearMatrix/rowmatrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+A = np.array([[1,2,3,4]])
+print(A)
+print(A.transpose())
+print(A.T)
+
+print('--------------')
+u = np.array([[1,2,3]]).T
+v = np.array([[5,6,7]]).T
+print(u + v)
+
